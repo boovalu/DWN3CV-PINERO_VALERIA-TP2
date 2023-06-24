8d3b568094bd930ca364ef3b7b034f11d576cd03
ValeriaPiñero <68872011+boovalu@users.noreply.github.com>
2023-06-23 22:09:17 -0300

    Update register-sw.js

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,6 +1,6 @@
 // Chequeo si el browser soport Service Worker
 if ('serviceWorker' in navigator){
-    navigator.serviceWorker.register('../serviceworker.js').then((message)=>{
+    navigator.serviceWorker.register('serviceworker.js').then((message)=>{
         console.log('Service worker esta OK y en acción!');
     })
 } else { // no soporta Service worker el browser
